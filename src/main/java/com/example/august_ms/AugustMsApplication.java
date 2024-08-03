455483671aa51bd042ec7a254242c95b312a7936
ilasingh20 <ns.ilasingh@gmail.com>
2024-08-03 11:21:56 +0530

    push docker image

---
+++ src/main/java/com/example/august_ms/AugustMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.august_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AugustMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AugustMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/august_ms/AugustMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.august_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AugustMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
